09cb59401074e6954ed1089fa1eedf013daa07cb
Evert Ingwersen <everti2000@gmail.com>
2026-03-06 13:54:31 +0100

    update script description

--- Modeling/Rail_map_Scandinavia.py
+++ Modeling/Rail_map_Scandinavia.py
@@ -3,6 +3,7 @@
 Scandinavia rail network 
 - plots edges colored by maxspeed
 - finds fastest route between any DK, SE and NO city
+- intermediate stops possible
 - border connections manually added
 """
 
