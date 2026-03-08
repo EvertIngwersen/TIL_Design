ae03afae8ea85fdde07fc4d057036ed3d015be82
Evert Ingwersen <everti2000@gmail.com>
2026-03-08 13:07:38 +0100

    removed param comment

--- Modeling/parameters.py
+++ Modeling/parameters.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-parameters_bidirectional.py
-
 Parameters for HSR-Airport integration model with BIDIRECTIONAL transfers.
 Includes both outgoing and incoming flights.
 """
