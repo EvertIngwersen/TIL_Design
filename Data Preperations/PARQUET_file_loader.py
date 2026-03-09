5e329d5f96e72e500e70959e689517dade6dd4c6
Evert Ingwersen <everti2000@gmail.com>
2026-03-09 16:03:15 +0100

    made parquet file loader

--- Data Preperations/PARQUET_file_loader.py
+++ Data Preperations/PARQUET_file_loader.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  9 14:12:53 2026
+from pathlib import Path
+import pyarrow.parquet as pq
 
-@author: evert
-"""
+# Current script directory
+BASE_DIR = Path(__file__).resolve().parent
+
+# Relative path to the parquet file
+parquet_file = BASE_DIR.parent / "Large Data" / "flight_events" / "flight_events_20240101_20240111.parquet"
+
+print("Scanning parquet file from:", parquet_file)
+
+# Open the parquet file as a PyArrow Table
+table = pq.read_table(parquet_file, columns=["flight_id", "timestamp", "lat", "lon"])
+
+# Convert to Pandas DataFrame if needed
+df = table.to_pandas()
+
+# Inspect first rows
+print(df.head())
+print(df.info())
 
