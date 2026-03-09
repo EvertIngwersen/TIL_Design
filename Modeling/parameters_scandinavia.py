e794bee95b3581869d151e00a1ec433667309a63
Evert Ingwersen <everti2000@gmail.com>
2026-03-09 16:35:14 +0100

    modified flight number parameters

--- Modeling/parameters_scandinavia.py
+++ Modeling/parameters_scandinavia.py
@@ -116,9 +116,9 @@ def generate_incoming_flights_per_airport_random(airport_settings, max_variation
 
 # Example usage:
 airport_settings_incoming = {
-    1: {'num_flights': 20, 'start_time': 420, 'end_time': 1380},  # 7:00 - 23:00
-    2: {'num_flights': 15, 'start_time': 480, 'end_time': 1320},  # 8:00 - 22:00
-    3: {'num_flights': 10, 'start_time': 450, 'end_time': 1200},  # 7:30 - 20:00
+    1: {'num_flights': 5, 'start_time': 420, 'end_time': 1380},  # 7:00 - 23:00
+    2: {'num_flights': 8, 'start_time': 480, 'end_time': 1320},  # 8:00 - 22:00
+    3: {'num_flights': 6, 'start_time': 450, 'end_time': 1200},  # 7:30 - 20:00
 }
 
 incoming_flights = generate_incoming_flights_per_airport_random(airport_settings_incoming, max_variation=5)
@@ -163,7 +163,7 @@ trains = {
 }
 
 # How many extra trains per route
-extra_per_route = 4
+extra_per_route = 2
 
 # Find the current max train ID to continue numbering
 max_id = max(trains.keys())
