6ba8c25a5cfc5132b85485dca70ae040a95d1fe2
Evert Ingwersen <everti2000@gmail.com>
2026-03-09 13:29:00 +0100

    added airport loc in param

--- Modeling/parameters_scandinavia.py
+++ Modeling/parameters_scandinavia.py
@@ -9,9 +9,9 @@ Created on Mon Mar  9 13:25:40 2026
 # 0. STATIONS
 # ==========================================================
 stations = {
-    1: {"coords": (0, 0)},
-    2: {"coords": (1, 0.5)},
-    3: {"coords": (2, 1), "airport": True}
+    1: {"coords": (17.9331197, 59.652312), "airport": True},   #Arlanda Airport
+    2: {"coords": (12.6494168, 55.6296397), "airport": True},  #Kastrup Airport
+    3: {"coords": (11.0967803, 60.1929196), "airport": True}   #Gardermoen Airport
     }
 
 
