35b657593339c6b0f55978eefabdc3a369488c47
Evert Ingwersen <everti2000@gmail.com>
2026-03-12 16:17:02 +0100

    added txt box in form designer

--- GUI/RailRouteGUI/Form1.Designer.cs
+++ GUI/RailRouteGUI/Form1.Designer.cs
@@ -35,6 +35,45 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
+        // Origin
+        this.txtOrigin = new System.Windows.Forms.TextBox();
+        this.txtOrigin.Location = new System.Drawing.Point(20, 20);
+        this.txtOrigin.Size = new System.Drawing.Size(200, 22);
+        this.txtOrigin.PlaceholderText = "Origin City";
+        this.Controls.Add(this.txtOrigin);
+
+        // Via
+        this.txtVia = new System.Windows.Forms.TextBox();
+        this.txtVia.Location = new System.Drawing.Point(20, 60);
+        this.txtVia.Size = new System.Drawing.Size(200, 22);
+        this.txtVia.PlaceholderText = "Via (comma-separated)";
+        this.Controls.Add(this.txtVia);
+
+        // Destination
+        this.txtDestination = new System.Windows.Forms.TextBox();
+        this.txtDestination.Location = new System.Drawing.Point(20, 100);
+        this.txtDestination.Size = new System.Drawing.Size(200, 22);
+        this.txtDestination.PlaceholderText = "Destination City";
+        this.Controls.Add(this.txtDestination);
+
+        // Button
+        this.btnCalculate = new System.Windows.Forms.Button();
+        this.btnCalculate.Location = new System.Drawing.Point(20, 140);
+        this.btnCalculate.Size = new System.Drawing.Size(200, 30);
+        this.btnCalculate.Text = "Calculate Route";
+        this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+        this.Controls.Add(this.btnCalculate);
+
+        // Result Label
+        this.lblResult = new System.Windows.Forms.Label();
+        this.lblResult.Location = new System.Drawing.Point(20, 190);
+        this.lblResult.Size = new System.Drawing.Size(600, 400);
+        this.lblResult.AutoSize = false;
+        this.lblResult.Text = "Route summary will appear here";
+        this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        this.lblResult.BackColor = System.Drawing.Color.White;
+        this.lblResult.Padding = new Padding(5);
+        this.Controls.Add(this.lblResult);
         components = new System.ComponentModel.Container();
         AutoScaleMode = AutoScaleMode.Font;
         ClientSize = new Size(800, 450);
