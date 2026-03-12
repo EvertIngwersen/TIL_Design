db5a088d88be2593eda483ae4044c1a0fa245b70
Evert Ingwersen <everti2000@gmail.com>
2026-03-12 16:16:02 +0100

    added new project for rail route GUI

---
+++ GUI/RailRouteGUI/Form1.Designer.cs
@@ -0,0 +1,45 @@
+﻿
+private System.Windows.Forms.TextBox txtOrigin;
+private System.Windows.Forms.TextBox txtVia;
+private System.Windows.Forms.TextBox txtDestination;
+private System.Windows.Forms.Button btnCalculate;
+private System.Windows.Forms.Label lblResult;
+
+namespace RailRouteGUI;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        components = new System.ComponentModel.Container();
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Text = "Form1";
+    }
+
+    #endregion
+}
