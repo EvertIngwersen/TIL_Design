e1a7b1958eb17c6c73941b6ecad97509cb970ed8
Evert Ingwersen <everti2000@gmail.com>
2026-03-12 16:36:04 +0100

    modified program.cs file for VS code compatible

--- GUI/RailRouteGUI/Program.cs
+++ GUI/RailRouteGUI/Program.cs
@@ -1,16 +1,16 @@
-namespace RailRouteGUI;
+using System;
+using System.Windows.Forms;
 
-static class Program
+namespace RailRouteGUI
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    static class Program
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
-    }    
+        [STAThread] // This is necessary for WinForms
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
 }
